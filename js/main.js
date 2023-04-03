6caedd292cd5363e7fbe25f1864ce9aa77057739
Veeedar <vedar@gmail.com>
2023-04-03 11:46:26 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,173 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  
+  });
+ 
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.231452671832756, lng: -8.628391974899195},
+    zoom: 14,
+    styles:[
+      {
+          "featureType": "administrative",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 65
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": "50"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "30"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "40"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffff00"
+              },
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -97
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -100
+              }
+          ]
+      }
+  ]
+  });
+  // Define the LatLng coordinates for the outer path.
+  const outerCoords = [
+    { lat: -32.364, lng: 153.207 },
+    { lat: -35.364, lng: 153.207 },
+    { lat: -35.364, lng: 158.207 },
+    { lat: -32.364, lng: 158.207 }, // north east
+  ];
+  // Define the LatLng coordinates for an inner path.
+  const innerCoords1 = [
+    { lat: -33.364, lng: 154.207 },
+    { lat: -34.364, lng: 154.207 },
+    { lat: -34.364, lng: 155.207 },
+    { lat: -33.364, lng: 155.207 },
+  ];
+  // Define the LatLng coordinates for another inner path.
+  const innerCoords2 = [
+    { lat: -33.364, lng: 156.207 },
+    { lat: -34.364, lng: 156.207 },
+    { lat: -34.364, lng: 157.207 },
+    { lat: -33.364, lng: 157.207 },
+  ];
+
+  map.data.add({
+    geometry: new google.maps.Data.Polygon([
+      outerCoords,
+      innerCoords1,
+      innerCoords2,
+    ]),
+  });
+}
+
+window.initMap = initMap;
